781c385db03d1bba38b7fdf5b30fa632586d7bc7
Chrinkus <seeschickrun@gmail.com>
2018-12-26 23:05:38 -0600

    Completed Chapter 1.
    Not sure how this repo will go. There isn't much in the way of problem
    solving, just typing out the examples. Getting them to run, however, is
    not trivial and will only get more complicated.
    
    Also, some examples do NOT use the 'best' available language features so
    there are opportunities to change some of the sources.

---
+++ ch01_todo/mainwindow.cpp
@@ -0,0 +1,70 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "task.h"
+
+#include <QDebug>
+#include <QInputDialog>
+#include <algorithm>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    mTasks()
+{
+    ui->setupUi(this);
+    connect(ui->addTaskButton, &QPushButton::clicked,
+            this, &MainWindow::addTask);
+    updateStatus();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::addTask()
+{
+    bool ok;
+    QString name = QInputDialog::getText(this,
+                                         tr("Add task"),
+                                         tr("Task name"),
+                                         QLineEdit::Normal,
+                                         tr("Untitled task"),
+                                         &ok);
+    if (ok && !name.isEmpty()) {
+        qDebug() << "Adding new task";
+        Task* task = new Task(name);
+        connect(task, &Task::removed, this, &MainWindow::removeTask);
+        connect(task, &Task::statusChanged, this,
+                &MainWindow::taskStatusChanged);
+        mTasks.append(task);
+        ui->tasksLayout->addWidget(task);
+        updateStatus();
+    }
+}
+
+void MainWindow::removeTask(Task* task)
+{
+    mTasks.removeOne(task);
+    ui->tasksLayout->removeWidget(task);
+    task->setParent(nullptr);
+    delete task;
+    updateStatus();
+}
+
+void MainWindow::taskStatusChanged(Task*)
+{
+    updateStatus();
+}
+
+void MainWindow::updateStatus()
+{
+    auto completedCount = std::count_if(std::begin(mTasks), std::end(mTasks),
+                   [](const auto t) { return t->isCompleted(); });
+
+    auto todoCount = mTasks.size() - completedCount;
+    ui->statusLabel->setText(
+                QString("Status: %1 todo / %2 completed")
+                    .arg(todoCount)
+                    .arg(completedCount));
+}
